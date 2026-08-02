d9fd3e48bd0a679abb1afc4de75100726f1e258f
Priyanshu Jha <anshu24005@gmail.com>
2026-08-02 20:48:33 +0530

    Time: 0 ms (100%), Space: 47.9 MB (57.81%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,21 @@
+class Solution {
+     double ans = 1;
+
+    public double solve(double x, int n) {
+
+        if (n == 0) return 1;
+
+        ans = solve(x, n / 2);
+
+        if (n % 2 == 0) return ans * ans;
+
+        return ans * ans * x;
+    }    
+    public double myPow(double x, int n) {
+        if (n == 0) return 1.0;
+
+        return n < 0
+                ? 1.0 / solve(x, -n)
+                : solve(x, n);
+    }
+}
