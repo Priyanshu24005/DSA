471a29d1364aa0c69f2732fc0ae73827d43a99ed
Priyanshu Jha <anshu24005@gmail.com>
2026-08-22 17:42:48 +0530

    Time: 0 ms (100%), Space: 44.7 MB (48.73%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -8,27 +8,13 @@ class Solution {
 
             if (nums[mid] == target) {
                 return mid;
-            } 
-            else if (nums[mid] > target) {
-
-                if (mid == 0 || nums[mid - 1] < target) {
-                    return mid;
-                } 
-                else {
-                    l = mid - 1;
-                }
-            } 
-            else {
-
-                if (mid == nums.length - 1 || nums[mid + 1] > target) {
-                    return mid + 1;
-                } 
-                else {
-                    f = mid + 1;
-                }
+            } else if (nums[mid] > target) {
+                l = mid - 1;
+            } else {
+                f = mid + 1;
             }
         }
 
-        return nums.length;
+        return f;
     }
 }
